da8db3cd066befbb5e4375c703862e204c32b396
Jake Coble <jakecoble@users.noreply.github.com>
2019-03-06 21:28:45 -0800

    put a timer in the button

--- flip.js
+++ flip.js
@@ -10,6 +10,36 @@ var config = {
 var resultDisplay = document.querySelector('#resultDisplay')
 var flipButton = document.querySelector('#flipButton')
 
+var secondsLeft = 0
+var timerId
+
+function formatNumber (num) {
+  var padding = ''
+
+  if (num < 10) {
+    padding = '0'
+  }
+
+  return padding + num.toString()
+}
+
+function tick (start) {
+  let delta = Math.floor((Date.now() - start) / 1000)
+  let remaining = config.disabledSeconds - delta
+
+  if (remaining <= 0) {
+    clearInterval(timerId)
+    flipButton.disabled = false
+    resultDisplay.innerHTML = ''
+    flipButton.innerHTML = 'flip'
+  } else {
+    let minutes = Math.floor(remaining / 60)
+    let seconds = remaining % 60
+
+    flipButton.innerHTML = minutes + ':' + formatNumber(seconds)
+  }
+}
+
 function flip () {
   var heads = Math.floor(Math.random() * 100) + 1 < config.headsOdds
 
@@ -23,10 +53,9 @@ function flip () {
   }
 
   flipButton.disabled = true
-  setTimeout(function () {
-    flipButton.disabled = false
-    resultDisplay.innerHTML = ''
-  }, config.disabledSeconds * 1000)
+  var timerStart = Date.now()
+  flipButton.innerHTML = '5:00'
+  timerId = setInterval(tick, 1000, timerStart)
 }
 
 flipButton.addEventListener('click', function (event) {
