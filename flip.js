e0e3d19a9b414c9f0c213b9dd3ee85b2bef5837f
Jake Coble <jakecoble@users.noreply.github.com>
2019-03-06 17:57:34 -0800

    initial commit, write basic app with no styles

---
+++ flip.js
@@ -0,0 +1,33 @@
+var config = {
+  headsOdds: 50,
+  disabledSeconds: 300,
+  headsMessage: 'Facebook time!',
+  tailsMessage: 'No Facebook right now – face your fears!'
+}
+
+var resultDisplay = document.querySelector('#resultDisplay')
+var flipButton = document.querySelector('#flipButton')
+
+function flip () {
+  var heads = Math.floor(Math.random() * 100) + 1 < config.headsOdds
+
+  resultDisplay.innerHTML = heads ? config.headsMessage : config.tailsMessage
+  if (heads) {
+    let fb = window.open('https://www.facebook.com')
+
+    if (fb && !fb.closed) {
+      fb.focus()
+    }
+  }
+
+  flipButton.disabled = true
+  setTimeout(function () {
+    flipButton.disabled = false
+  }, config.disabledSeconds * 1000)
+}
+
+flipButton.addEventListener('click', function (event) {
+  event.preventDefault()
+
+  flip()
+})
